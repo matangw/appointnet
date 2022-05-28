fda843ca3d41daf96fa128a31daf6add670cc706
User <matangw@gmail.com>
2022-05-29 00:15:11 +0300

    with event repository

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,10 +1,35 @@
-class event{
+import 'package:appointnet/models/parlament.dart';
+import 'package:uuid/uuid.dart';
 
-  String id;
+class Event{
+
+  String? id;
   DateTime date;
   String location;
   List<String> attendingsIds;
 
+  Event({required this.date,required this.location,required this.attendingsIds, this.id,}){
+    id??= Uuid().v4();
+    attendingsIds??= [];
+  }
 
+  Map<String,dynamic> toJson(){
+    return {
+      'id': id,
+      'date': date,
+      'location': location,
+      'attendings': attendingsIds
+    };
+  }
+
+
+  factory Event.fromJson(Map<String,dynamic> json){
+    return Event(
+        id: json['id'],
+        date: DateTime.parse(json['date']),
+        location: json['location'],
+        attendingsIds: json['attendings'] == null ? [] : json['attendings']?.cast<String>(),
+    );
+  }
 
 }
