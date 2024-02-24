f755381a2954d0f0487288ff97e4b96c1489e64d
matangabso <matangw@gmail.com>
2024-02-24 21:47:57 +0200

    a

--- lib/models/bring_item.dart
+++ lib/models/bring_item.dart
@@ -1,26 +1,32 @@
 import 'package:uuid/uuid.dart';
 
-class BringItem{
+class BringItem {
   String name;
   int quantity;
   String? id;
   bool parlamental;
 
-
-  BringItem({required this.name,required this.quantity,required this.parlamental ,this.id}){
-   id??=Uuid().v4();
+  BringItem(
+      {required this.name,
+      required this.quantity,
+      required this.parlamental,
+      this.id}) {
+    id ??= Uuid().v4();
   }
 
-  factory BringItem.fromJson(Map<String,dynamic> json){
-    return BringItem(name: json['name'], quantity: json['quantity'],parlamental: json['parlamental'],id: json['id']);
+  factory BringItem.fromJson(Map<String, dynamic> json) {
+    return BringItem(
+        name: json['name'],
+        quantity: json['quantity'],
+        parlamental: json['parlamental'],
+        id: json['id']);
   }
 
-  Map<String,dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {
       'id': this.id,
       'quantity': this.quantity,
       'name': this.name,
-      
       'parlamental': this.parlamental
     };
   }
